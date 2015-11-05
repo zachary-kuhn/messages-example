9128cc15570415f7912f85d864c51b440e9177a3
Zachary Kuhn <zachary.kuhn@gmail.com>
2015-11-05 10:51:17 -0500

    Convert to a message writing app.

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,24 @@
 module.exports = {
   entry: {
-    app: ['./app/main.js']
+    app: ['./app/main']
   },
   output: {
     path: './app',
     filename: 'bundle.js'
+  },
+  resolve: {
+    extensions: [ '', '.js', '.jsx' ]
+  },
+  module: {
+    loaders: [{
+      test: /\.jsx?$/,
+      loader: 'babel',
+      query: {
+        presets: ['es2015', 'stage-0', 'react']
+      }
+    }, {
+      test: /\.css$/,
+      loader: 'style-loader!css-loader'
+    }]
   }
 };
